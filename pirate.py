9e4e5f1419bab7fbe9ec2aef3014e91d3f7005f3
Jaime Salcedo <91972625+jsalcedo1@users.noreply.github.com>
2021-10-23 21:33:31 -0500

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,31 @@
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ("Arrr!"):
+	print("Go away, pirate.")
+else:
+	print("Greetings, hater of pirates!")
+
+'''
+Edited by: Jaime S.
+10/22/2021
+
+Corrections: 
+==================================================================================
+Line 1 has 1 error
+
+Line 1: Syntax Error: Missing a double quote ", a double quote "  should be added
+==================================================================================
+Line 2 has 1 error
+
+Line 2: Syntax Error: Wrong square bracket [, a parenthesis ( should be added instead 
+
+==================================================================================
+Line 4 has 2 error
+
+Line 4: Syntax Error: Wrong expression elif statement, the else statement should be added instead
+Line 4: Syntax Error: Missing a colon after else, a colon : should be added after else:
+===================================================================================
+Line 5 has 1 error
+
+Line 5: Syntax Error: Missing tab spacing, tab spacing should be added
+====================================================================================
+'''
---
+++ time2.py
@@ -0,0 +1,25 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of hours to wait?")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
+
+
+'''
+Edited by: Jaime S.
+10/22/2021
+
+Corrections: 
+===================================================================
+Line 2 has 1 error
+
+Line 2:Syntax Error: Misspelled word for hours, it should say hours
+====================================================================
+Line 4 has 1 error
+
+Line 4: Misspelled word for wait, a t should be added to wait 
+=====================================================================
+
+'''
